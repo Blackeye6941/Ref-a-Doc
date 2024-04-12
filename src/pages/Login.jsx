eba214cf5979b0049df1beb54d8d52d6df32fc35
Blackeye6941 <leok37316@gmail.com>
2024-04-13 03:12:36 +0530

    Login updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,8 +8,8 @@ const Login = () => {
         <div className=' mx-[30rem] pb-[2rem] mt-5 rounded-[30px] border-2 border-red'>
             <h1 className='text-[40px] py-10 font-mono text-center text-red'>Sign In</h1>
             <form action="">
-                <label htmlFor="" className='text-[25px] pt-[30px] pl-5'>Name: </label>
-                <input type="text" placeholder='Enter the Name' id="name" className='block mx-5 py-[2px] my-4 text-[25px] rounded-[10px] border-2 border-red pl-6 pr-[13rem]' />
+                <label htmlFor="" className='text-[25px] pt-[30px] pl-5'>Email: </label>
+                <input type="Email" placeholder='Enter the Name' id="name" className='block mx-5 py-[2px] my-4 text-[25px] rounded-[10px] border-2 border-red pl-6 pr-[13rem]' />
                 <label htmlFor="" className='text-[25px] pt-[30px] pl-5 mb-[20px]'>Password: </label>
                 <input type="password" placeholder='Enter the Password' id="name" className='block mx-5 my-4 py-[2px] text-[25px] rounded-[10px] border-2 border-red pl-6 pr-[13rem] ' />
 
