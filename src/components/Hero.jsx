2a201af9f9f7a1d6ccc617cfdaba7c770fa24f52
Pranav M <pranavm265@gmail.com>
2024-04-13 15:31:26 +0530

    Animated Home hehe

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-
+import React from 'react';
+import './Hero.css'; // Import CSS file using relative path
 
 const Hero = () => {
   return (
-    <>
-      <h1 className='text-[4.5rem] font-sans pt-10 font-semibold '>Doctor Not Available??</h1>
-      <h1 className='text-[4.5rem] font-semibold px-[15rem]'>Don't Worry</h1>
-      <h1 className='text-[4.5rem] font-semibold px-[27.5rem]'>We got you  !!!</h1>
-    </>
-  )
-}
+    <div className='hero-container'>
+      <h1 className='text-animation text-[4.5rem] font-sans font-semibold '>Doctor Not Available??</h1>
+      <h1 className='text-animation text-[4.5rem] font-semibold  px-[15rem]'>Don't Worry</h1>
+      <h1 className='text-animation text-[4.5rem] font-semibold px-[27.5rem]'>We got you  !!!</h1>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
