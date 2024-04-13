e0e985419f366d88507e9efa34e00a5e303d20ff
Blackeye6941 <leok37316@gmail.com>
2024-04-14 01:13:48 +0530

    Added login and register forms for api integration

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email:'',
+    password:'',
+  });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDoctorLogin = () => {
+    // Implement doctor login logic here
+  };
+
+  const handlePatientLogin = () => {
+    // Implement patient login logic here
+  };
+
   return (
     <>
       <div>
         <div className=' mx-[30rem] pb-[2rem] mt-5 rounded-[30px] border-2 border-red'>
             <h1 className='text-[40px] py-10 font-mono text-center text-red'>Sign In</h1>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="" className='text-[25px] pt-[30px] pl-5'>Email: </label>
-                <input type="Email" placeholder='Enter the Name' id="name" className='block mx-5 py-[2px] my-4 text-[25px] rounded-[10px] border-2 border-red pl-6 pr-[13rem]' />
+                <input type="Email" placeholder='Enter the Name' id="name" className='block mx-5 py-[2px] my-4 text-[25px] rounded-[10px] border-2 border-red pl-6 pr-[13rem]' name='email' onChange={handleInputChange}/>
                 <label htmlFor="" className='text-[25px] pt-[30px] pl-5 mb-[20px]'>Password: </label>
-                <input type="password" placeholder='Enter the Password' id="name" className='block mx-5 my-4 py-[2px] text-[25px] rounded-[10px] border-2 border-red pl-6 pr-[13rem] ' />
+                <input type="password" placeholder='Enter the Password' id="name" className='block mx-5 my-4 py-[2px] text-[25px] rounded-[10px] border-2 border-red pl-6 pr-[13rem] ' name='password' onChange={handleInputChange}/>
 
-                 <Link><button className='bg-red text-white text-[25px] px-5  py-[3px] ml-[20px] mt-[50px] rounded-[10px] hover:bg-lightred'>Login as Doctor</button></Link>
-                 <Link><button className='bg-red text-white text-[25px] px-5  py-[3px] ml-[90px] mt-[50px] rounded-[10px] hover:bg-lightred'>Login as Patient</button></Link>
+                 <Link><button className='bg-red text-white text-[25px] px-5  py-[3px] ml-[20px] mt-[50px] rounded-[10px] hover:bg-lightred' onClick={handleDoctorLogin}>Login as Doctor</button></Link>
+                 <Link><button className='bg-red text-white text-[25px] px-5  py-[3px] ml-[90px] mt-[50px] rounded-[10px] hover:bg-lightred' onClick={handlePatientLogin}>Login as Patient</button></Link>
                  <h1 className='text-[20px] pt-[20px] pl-6'>Don't have an account? <Link to='/register' className='text-blue underline'>Register Here</Link></h1>
             </form>
         </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,35 +1,93 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
 const Register = () => {
-
   const [userType, setUserType] = useState('patient'); // Default user type is patient
+  const [formData, setFormData] = useState({
+    name: '',
+    email:'',
+    phoneNumber: '',
+    age: '',
+    userType: 'patient',
+    specialization: '',
+    hospital: '',
+    password: ''
+  });
 
   const handleUserTypeChange = (event) => {
     setUserType(event.target.value);
   };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <div className='flex'>
-        <div className='bg-red w-[40%] text-center py-[18rem]' >
-        </div>
+        <div className='bg-red w-[40%] text-center py-[18rem]'></div>
         <div className='inline '>
-           <h1 className='text-[2.5em] font-mono pl-8 pt-10'>Register</h1>
-           <h1 className='text-[1.5rem] pl-8 pt-8'>Create an account here..</h1>
-           <h1 className='pl-8 pt-4 text-[20px] pb-10'>Enter the details below</h1>
-           <form>
+          <h1 className='text-[2.5em] font-mono pl-8 pt-10'>Register</h1>
+          <h1 className='text-[1.5rem] pl-8 pt-8'>Create an account here..</h1>
+          <h1 className='pl-8 pt-4 text-[20px] pb-10'>Enter the details below</h1>
+          <form onSubmit={handleSubmit}>
             <div className='flex md:flex-col'>
-                <label htmlFor="" className="pl-8 text-[20px] pt-10 pb-5">Name:</label>
-                <input type="text" placeholder='Enter your name' className="pl-2 text-[20px] border-2 ml-5 w-[40rem] rounded-xl py-2" />
+              <label htmlFor='' className='pl-8 text-[20px] pt-10 pb-5'>
+                Name:
+              </label>
+              <input
+                type='text'
+                placeholder='Enter your name'
+                className='pl-2 text-[20px] border-2 ml-5 w-[40rem] rounded-xl py-2'
+                value={formData.name}
+                name='name'
+                onChange={handleInputChange}
+              />
             </div>
             <div className='flex md:flex-col'>
-                <label htmlFor="" className="pl-8 text-[20px] pt-10 pb-5">Phone Number:</label>
-                <input type="text" placeholder='Enter your phone Number' className=" py-2 pl-2 text-[20px] border-2 ml-5 w-[40rem] rounded-xl" />
+              <label htmlFor='' className='pl-8 text-[20px] pt-10 pb-5'>
+                Email:
+              </label>
+              <input
+                type='email'
+                placeholder='Enter your email'
+                className='pl-2 text-[20px] border-2 ml-5 w-[40rem] rounded-xl py-2'
+                value={formData.email}
+                name='email'
+                onChange={handleInputChange}
+              />
             </div>
             <div className='flex md:flex-col'>
-                <label htmlFor="" className="pl-8 text-[20px] pt-10 pb-5">Age:</label>
-                <input type="number" placeholder='Enter your age' className="py-2 pl-2 text-[20px] border-2 ml-5 rounded-xl pr-[40px] w-[40rem]" />
+              <label htmlFor='' className='pl-8 text-[20px] pt-10 pb-5'>
+                Phone Number:
+              </label>
+              <input
+                type='text'
+                placeholder='Enter your phone Number'
+                className=' py-2 pl-2 text-[20px] border-2 ml-5 w-[40rem] rounded-xl'
+                value={formData.phoneNumber}
+                name='phoneNumber'
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className='flex md:flex-col'>
+              <label htmlFor='' className='pl-8 text-[20px] pt-10 pb-5'>
+                Age:
+              </label>
+              <input
+                type='number'
+                placeholder='Enter your age'
+                className='py-2 pl-2 text-[20px] border-2 ml-5 rounded-xl pr-[40px] w-[40rem]'
+                value={formData.age}
+                name='age'
+                onChange={handleInputChange}
+              />
             </div>
             <div className='flex md:flex-col'>
               <label htmlFor='' className='pl-8 text-[20px] pt-10 pb-5'>
@@ -57,6 +115,9 @@ const Register = () => {
                     type='text'
                     placeholder='Enter your specialization'
                     className='py-2 pl-2 text-[20px] border-2 ml-5 w-[40rem] rounded-xl'
+                    value={formData.specialization}
+                    name='specialization'
+                    onChange={handleInputChange}
                   />
                 </div>
                 <div className='flex md:flex-col'>
@@ -70,22 +131,41 @@ const Register = () => {
                     type='text'
                     placeholder='Enter your hospital'
                     className='py-2 pl-2 text-[20px] border-2 ml-5 w-[40rem] rounded-xl'
+                    value={formData.hospital}
+                    name='hospital'
+                    onChange={handleInputChange}
                   />
                 </div>
               </>
             )}
 
             <div className='flex md:flex-col'>
-                <label htmlFor="" className="pl-8 text-[20px] pt-10 pb-5">Password:</label>
-                <input type="password" placeholder='Enter your password' className="py-2 pl-2 text-[20px] border-2 ml-5 mb-10 pr-[40px] rounded-xl w-[40rem]" min='8'/>
+              <label htmlFor='' className='pl-8 text-[20px] pt-10 pb-5'>
+                Password:
+              </label>
+              <input
+                type='password'
+                placeholder='Enter your password'
+                className='py-2 pl-2 text-[20px] border-2 ml-5 mb-10 pr-[40px] rounded-xl w-[40rem]'
+                value={formData.password}
+                name='password'
+                onChange={handleInputChange}
+              />
             </div>
-            <button className="rounded-xl ml-[18rem] my-8 text-white bg-red hover:bg-light-red mb-[20px] text-[20px] py-2 px-5">Sign Up</button>
-           </form>
-           <h1 className='text-[20px] pb-[20px] pl-10'>Already have an account? <Link to='/login' className='text-blue'>Login here</Link></h1>
+            <button className='rounded-xl ml-[18rem] my-8 text-white bg-red hover:bg-light-red mb-[20px] text-[20px] py-2 px-5'>
+              Sign Up
+            </button>
+          </form>
+          <h1 className='text-[20px] pb-[20px] pl-10'>
+            Already have an account?{' '}
+            <Link to='/login' className='text-blue'>
+              Login here
+            </Link>
+          </h1>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
